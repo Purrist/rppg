5d26069935eef73963e0db1e87f7f3d4060f9958
purrist <2633705241@qq.com>
2026-01-14 09:39:57 +0800

    1.0（基础）

---
+++ backend/localapp.py
@@ -0,0 +1,10 @@
+from app import app
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8080))
+    app.run(
+        host='0.0.0.0',   # 关键点
+        port=port,
+        debug=True
+    )
